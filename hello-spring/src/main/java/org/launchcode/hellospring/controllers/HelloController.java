8e6aabdcb218f0036190212d2caa2dd361f42c48
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-30 03:04:27 -0400

    Allowed for form submission by POST request

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,12 +1,8 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 
 @Controller
 public class HelloController {
@@ -26,7 +22,7 @@ public class HelloController {
     }
 
     // Handles request of the form /hello?name=LaunchCode
-    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
@@ -44,7 +40,7 @@ public class HelloController {
     public String helloForm() {
         return "<html>" +
                     "<body>" +
-                        "<form action='hello'>" +
+                        "<form action='hello' method='post'>" +
                             "<input type='text' name='name'>" +
                             "<input type='submit' value='Greet me!'>" +
                         "</form>" +

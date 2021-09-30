e7730a36a86812268d952bd54755553dbdd91111
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-30 02:34:30 -0400

    Added ability to say hello using variable provided in path

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,8 +2,11 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 public class HelloController {
@@ -26,6 +29,13 @@ public class HelloController {
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 }
